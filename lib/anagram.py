be31eb713af6daceec13cdaf14d2881de3abd966
zippykitche <zippyk80@gmail.com>
2024-12-06 12:00:07 +0300

    complete p3 anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+       self.word = word.lower()
+
+    def match(self, list):
+        sorted_word = sorted(self.word)
+        return [word for word in list if sorted(word.lower()) == sorted_word]
